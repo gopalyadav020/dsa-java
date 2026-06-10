39b466c6b3f9306f8387804ddec4288154c8669b
gopal <gopalyadav0946@gmail.com>
2026-06-10 14:51:01 +0530

    add my file

---
+++ basic.java
@@ -0,0 +1,14 @@
+public class basic {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int[][] arr = new int[5][6];
+         for(int i =0;i<5;i++){
+            for(int j =0;j<6;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+         }
+            
+        
+    }
+}
