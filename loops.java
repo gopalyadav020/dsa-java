ede316f99b7f98863402d9ff370c2183109e8d18
gopal <gopalyadav0946@gmail.com>
2026-06-29 18:14:36 +0530

    Save local changes

--- loops.java
+++ loops.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter n");
         int n = sc.nextInt();
-        for (int i = 2; i <= n; i++) {
-            if (n % i == 0) {
-                System.out.println("number is composite");
-            }
+        int count = 0;
+        while (n != 0) {
+            n /= 10;
+            count++;
         }
+        System.out.println(count);
     }
-
 }
